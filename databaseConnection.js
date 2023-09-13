1e5893a6bc8862b3064f09ba9c618bd1c256ed25
kiffeur <emmanueldesong46@gmail.com>
2023-09-13 12:02:56 +0100

    modification dans server.js

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,11 +1,26 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+/*const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root', // Utilisateur MySQL
   password: '', // Mot de passe MySQL
   database: 'kammouv' // Nom de ta base de données
 });
+*/
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME, 
+  password: process.env.DB_PASSWORD,
+  password: process.env.DB_NAME, 
+
+//database: 'kammouv'  Nom de ta base de données
+waitForConnection: true,
+connectionList : 10,
+queueLimit : 0
+});
+
+
 
 connection.connect((err) => {
   if (err) {
