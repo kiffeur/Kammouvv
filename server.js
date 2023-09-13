40b019ef63719f5bee487ffcc61d49e005972262
kiffeur <emmanueldesong46@gmail.com>
2023-09-13 19:12:36 +0100

    modification json

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.p;
 
 const busRoutes = require('./routes/busRoutes'); // Importe les routes
 const agenceRoutes = require('./routes/agenceRoutes'); // Importe les routes
